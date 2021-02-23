76e09b96990d64c08c290f6943667a3d7a83f5b6
john <jjchen2019@gmail.com>
2021-02-22 22:41:32 -0700

    Added priority queue, still doesn't compile

---
+++ priorityQueue.cc
@@ -0,0 +1,56 @@
+#include <cmath>
+#include "priorityQueue.h"
+
+PriorityQueue::PriorityQueue(unsigned int capacity) {
+  capacity_ = capacity;
+  size_ = 0;
+  heap_ = new DataType[capacity + 1];
+  for(int i = 0; i < capacity; ++i)
+    heap_[i] = NULL;
+}
+
+PriorityQueue::~PriorityQueue() {
+  delete [] heap_;
+}
+
+unsigned int PriorityQueue::size() const {
+  return size_;
+}
+
+bool PriorityQueue::empty() const {
+  return size_ == 0;
+}
+
+bool PriorityQueue::full() const {
+  return size_ == capacity_;
+}
+
+bool PriorityQueue::add(DataType val) {
+  if(full()) return false;
+  heap_[++size_] = val;
+  int new_index = size_;
+  while(heap_[new_index] > heap_[new_index/2] && new_index/2 != 0) {
+    DataType temp = heap_[new_index];
+    heap_[new_index] = heap_[new_index/2];
+    heap_[new_index/2] = temp;
+    new_index = new_index / 2;
+  }
+  return true;
+}
+
+bool PriorityQueue::pop() {
+  if(empty()) return false;
+  heap_[1] = heap_[size_];
+  --size_;
+  int i = 1;
+  int larger;
+  while(2 * i < size_ && (heap_[i] < heap_[2*i] || heap_[i] < heap_[2*i+1])) {
+    larger = (heap_[2*i] > heap_[2*i+1]) ? 2 * i : 2 * i + 1;
+    DataType temp = heap_[i];
+    heap_[i] = heap_[larger];
+    heap_[larger] = temp;
+    i = larger;
+  }
+  return true;
+}
+
