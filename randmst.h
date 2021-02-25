e82da65935543d0fea00ae912fa2a33da78f0af3
Frank D'Agostino <frankdag20@gmail.com>
2021-02-25 03:14:00 -0500

    Added edge condition and added cutoff function k(n)

--- randmst.h
+++ randmst.h
@@ -7,42 +7,41 @@
 using namespace std;
 
 class Randmst {
-  public:
-  Randmst(int numPoints, int numDimensions);
-  ~Randmst();
-  struct node;
-  float prim(node *source_node, int dimensions, int n);
-  float get_distance(node *node1, node *node2, bool use_sqrt);
-  void generate_edges(vector<node*> nodes, float max_length);
-  vector<node*> generate_nodes(int dimensions, int points, unsigned int seed);
-  float get_run();
-  
-  struct coordinate {
-    vector<float> coordinates;
-  };
-  
-  struct closest_connected {
-    vector<node*> connected;
-  };
-  
-  struct close_nodes {
-    vector<node*> connected;
-  };
-  
-  struct node {
-    close_nodes *neighbor_nodes = nullptr;
-    coordinate *coordinates = nullptr;
-    node *closest_connected = nullptr;
-    float closest_distance = INFINITY;
+public:
+    Randmst(int numPoints, int flag, int numDimensions);
+    ~Randmst();
+    struct node;
+    float prim(node* source_node, int dimensions, int flag, int n);
+    float get_distance(node* node1, node* node2, bool use_sqrt);
+    void generate_edges(vector<node*> nodes, int flag, float max_length);
+    vector<node*> generate_nodes(int dimensions, int points, unsigned int seed);
+    float get_run();
 
-    node() {
-      close_nodes *new_neighbor = new close_nodes();
-      neighbor_nodes = new_neighbor;
-    }
-  };
+    struct coordinate {
+        vector<float> coordinates;
+    };
 
-  private:
+    struct closest_connected {
+        vector<node*> connected;
+    };
+
+    struct close_nodes {
+        vector<node*> connected;
+    };
+
+    struct node {
+        close_nodes* neighbor_nodes = nullptr;
+        coordinate* coordinates = nullptr;
+        node* closest_connected = nullptr;
+        float closest_distance = INFINITY;
+
+        node() {
+            close_nodes* new_neighbor = new close_nodes();
+            neighbor_nodes = new_neighbor;
+        }
+    };
+
+private:
     float run;
 };
 #endif
-
