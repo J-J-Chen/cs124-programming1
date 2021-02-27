e7e673e81b341362625ac3bb7a0ed662cb7b5c11
Frank D'Agostino <frankdag20@gmail.com>
2021-02-26 23:50:01 -0500

    Update priorityQueue.cc

--- priorityQueue.cc
+++ priorityQueue.cc
@@ -1,58 +1,93 @@
 #include <cmath>
+#include <stdio.h>
 #include "priorityQueue.h"
 
 PriorityQueue::PriorityQueue(unsigned int capacity) {
-  capacity_ = capacity;
-  size_ = 0;
-  heap_ = new Randmst::node[capacity + 1];
-  //for(int i = 0; i < capacity; ++i)
-    //heap_[i] = nullptr;
+    capacity_ = capacity;
+    size_ = 0;
+    heap_ = new Randmst::node * [capacity_ + 1];
+    for (int i = 0; i < capacity; ++i)
+        heap_[i] = nullptr;
 }
 
 PriorityQueue::~PriorityQueue() {
-  delete [] heap_;
+    delete[] heap_;
 }
 
 unsigned int PriorityQueue::size() const {
-  return size_;
+    return size_;
 }
 
 bool PriorityQueue::empty() const {
-  return size_ == 0;
+    return size_ == 0;
 }
 
 bool PriorityQueue::full() const {
-  return size_ == capacity_;
+    return size_ == capacity_;
 }
 
-bool PriorityQueue::add(Randmst::node val) {
-  if(full()) return false;
-  heap_[++size_] = val;
-  int new_index = size_;
-  while(heap_[new_index].closest_distance > heap_[new_index/2].closest_distance && new_index/2 != 0) {
-    Randmst::node temp = heap_[new_index];
-    heap_[new_index] = heap_[new_index/2];
-    heap_[new_index/2] = temp;
-    new_index = new_index / 2;
-  }
-  return true;
+bool PriorityQueue::add(Randmst::node* val) {
+    size_++;
+    int new_index = size_ - 1;
+    heap_[new_index] = val;
+    while (new_index != 0 && heap_[(new_index - 1) / 2]->closest_distance > heap_[new_index]->closest_distance) {
+        Randmst::node* temp = heap_[new_index];
+        heap_[new_index] = heap_[new_index / 2];
+        heap_[new_index / 2] = temp;
+        new_index = (new_index - 1) / 2;
+    }
+    return true;
 }
 
-Randmst::node PriorityQueue::pop() {
-  //if(empty()) return false;
-  heap_[1] = heap_[size_];
-  Randmst::node to_pop = heap_[size_];
-  --size_;
-  int i = 1;
-  int larger;
-  while(2 * i < size_ && (heap_[i].closest_distance < heap_[2*i].closest_distance || 
-        heap_[i].closest_distance < heap_[2*i+1].closest_distance)) {
-    larger = (heap_[2*i].closest_distance > heap_[2*i+1].closest_distance) ? 2 * i : 2 * i + 1;
-    Randmst::node temp = heap_[i];
-    heap_[i] = heap_[larger];
-    heap_[larger] = temp;
-    i = larger;
-  }
-  return to_pop;
+void PriorityQueue::resort(Randmst::node* val) {
+    int loc = 0;
+    for (int i = 0; i < size_; ++i) {
+        if (heap_[i] == val) {
+            loc = i;
+            break;
+        }
+    }
+
+    if (loc == -1) {
+        printf("NOOOO\n");
+        return;
+    }
+
+    while (heap_[loc]->closest_distance > heap_[(loc - 1) / 2]->closest_distance) {
+        Randmst::node* temp = heap_[(loc - 1) / 2];
+        heap_[(loc - 1) / 2] = heap_[loc];
+        heap_[loc] = temp;
+        loc = (loc - 1) / 2;
+        minHeapify(loc);
+    }
+    minHeapify(loc / 2);
 }
 
+void PriorityQueue::minHeapify(int i) {
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    int smallest = i;
+    if (l < size_ && heap_[l]->closest_distance < heap_[i]->closest_distance)
+        smallest = l;
+    if (r < size_ && heap_[r]->closest_distance < heap_[smallest]->closest_distance)
+        smallest = r;
+    if (smallest != i) {
+        Randmst::node* temp = heap_[i];
+        heap_[i] = heap_[smallest];
+        heap_[smallest] = temp;
+        minHeapify(smallest);
+    }
+}
+
+Randmst::node* PriorityQueue::pop() {
+    if (size_ == 0) return nullptr;
+    if (size_ == 1) {
+        --size_;
+        return heap_[0];
+    }
+    Randmst::node* to_pop = heap_[0];
+    heap_[0] = heap_[size_ - 1];
+    --size_;
+    minHeapify(0);
+    return to_pop;
+}
